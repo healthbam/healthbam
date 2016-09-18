6ad9c111dddf8c66d259b10f537e61c77beea8b2
Ryan Bard <john.ryan.bard@gmail.com>
2016-09-18 15:04:19 -0400

    closes #48: Add real IP to audit info
    * Adds HttpServletRequest to DefaultAuditHelper for obtaining real IP address.
    * Updates unit tests.

--- src/main/java/org/hmhb/audit/DefaultAuditHelper.java
+++ src/main/java/org/hmhb/audit/DefaultAuditHelper.java
@@ -1,22 +1,38 @@
 package org.hmhb.audit;
 
+import javax.annotation.Nonnull;
+import javax.servlet.http.HttpServletRequest;
+
 import java.util.Date;
 
 import com.codahale.metrics.annotation.Timed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static java.util.Objects.requireNonNull;
 
 @Service
 public class DefaultAuditHelper implements AuditHelper {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultAuditHelper.class);
 
+    private final HttpServletRequest request;
+
+    @Autowired
+    public DefaultAuditHelper(
+            @Nonnull HttpServletRequest request
+    ) {
+        LOGGER.debug("constructed");
+        this.request = requireNonNull(request, "request cannot be null");
+    }
+
     @Timed
     @Override
     public String getCallerIp() {
         LOGGER.debug("getCallerIp called");
-        return "127.0.0.1";
+        return request.getRemoteAddr();
     }
 
     @Override
--- src/test/java/org/hmhb/audit/DefaultAuditHelperTest.java
+++ src/test/java/org/hmhb/audit/DefaultAuditHelperTest.java
@@ -1,27 +1,36 @@
 package org.hmhb.audit;
 
+import javax.servlet.http.HttpServletRequest;
+
 import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Unit tests for {@link DefaultAuditHelper}.
  */
 public class DefaultAuditHelperTest {
 
+    private HttpServletRequest request;
     private DefaultAuditHelper toTest;
 
     @Before
     public void setUp() throws Exception {
-        toTest = new DefaultAuditHelper();
+        request = mock(HttpServletRequest.class);
+
+        toTest = new DefaultAuditHelper(request);
     }
 
     @Test
     public void testGetCallerIp() throws Exception {
         String expected = "127.0.0.1";
+
+        /* Train the mocks. */
+        when(request.getRemoteAddr()).thenReturn(expected);
 
         /* Make the call. */
         String actual = toTest.getCallerIp();
