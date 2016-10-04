f0097516b7f4d4b763e3c99046bdbea669851ec7
jmckoon3 <Jay.McKoon@gtri.gatech.edu>
2016-10-03 21:56:15 -0400

    Issue #103: Counties
    * Adds County, controller, service, and dao.
    * Adds insert script for "A" and "B" counties for now.
    * Adds find counties by name prefix ability.

---
+++ src/main/java/org/hmhb/county/CountyDao.java
@@ -0,0 +1,14 @@
+package org.hmhb.county;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Database access layer for {@link County} objects.
+ */
+public interface CountyDao extends CrudRepository<County, Long> {
+
+    List<County> findByNameStartingWithIgnoreCase(String namePart);
+
+}
---
+++ src/test/java/org/hmhb/county/DefaultCountyServiceTest.java
@@ -0,0 +1,94 @@
+package org.hmhb.county;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.hmhb.exception.county.CountyNotFoundException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests for {@link DefaultCountyService}.
+ */
+public class DefaultCountyServiceTest {
+
+    private static final long ID = 123L;
+    private static final String NAME = "test-name";
+    private static final String STATE = "test-state";
+    private static final String SHAPE = "test-shape";
+
+    private CountyDao dao;
+    private DefaultCountyService toTest;
+
+    @Before
+    public void setUp() throws Exception {
+        dao = mock(CountyDao.class);
+        toTest = new DefaultCountyService(dao);
+    }
+
+    private County createFilledInCounty() {
+        County county = new County();
+
+        county.setId(ID);
+        county.setName(NAME);
+        county.setState(STATE);
+        county.setShape(SHAPE);
+
+        return county;
+    }
+
+    @Test
+    public void testGetById() throws Exception {
+        County expected = createFilledInCounty();
+
+        /* Train the mocks. */
+        when(dao.findOne(ID)).thenReturn(expected);
+
+        /* Make the call. */
+        County actual = toTest.getById(ID);
+
+        /* Verify the results. */
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = CountyNotFoundException.class)
+    public void testGetByIdNotFound() throws Exception {
+        /* Train the mocks. */
+        when(dao.findOne(ID)).thenReturn(null);
+
+        /* Make the call. */
+        toTest.getById(ID);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        List<County> expected = Collections.singletonList(createFilledInCounty());
+
+        /* Train the mocks. */
+        when(dao.findAll()).thenReturn(expected);
+
+        /* Make the call. */
+        List<County> actual = toTest.getAll();
+
+        /* Verify the results. */
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindByNameStartingWithIgnoreCase() throws Exception {
+        List<County> expected = Collections.singletonList(createFilledInCounty());
+
+        /* Train the mocks. */
+        when(dao.findByNameStartingWithIgnoreCase(NAME)).thenReturn(expected);
+
+        /* Make the call. */
+        List<County> actual = toTest.findByNameStartingWithIgnoreCase(NAME);
+
+        /* Verify the results. */
+        assertEquals(expected, actual);
+    }
+
+}
